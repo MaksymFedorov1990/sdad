6b7247ab6ae251a68a3718dc2c7d8d19925d9d2e
Maxim Fedorov <FedorovMD1990@mail.ru>
2023-02-09 12:52:40 +0800

    finally

--- ref.py
+++ ref.py
@@ -5,3 +5,8 @@ print("Vasya")
 print("Solloveiko")
 >>>>>>> Stashed changes
 print("Nother of Stifler")
+<<<<<<< HEAD
+=======
+print("Roma loh")
+print("I telka u nego tupaya")
+>>>>>>> parent of c849d20 (modified)
